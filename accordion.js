939f8e1124db5b071b6ccadc097e0fbaee0ccb35
Pranshu <sahupranshu637@gmail.com>
2022-11-30 09:38:17 +0530

    improve functionality of accordion

--- accordion.js
+++ accordion.js
@@ -1,14 +1,14 @@
 const accordionHeaders = document.querySelectorAll("[data-accordion-header]")
 function toggleNav(e) {
-  closePreviousFaq()
-  console.log(e.currentTarget);
+  closePreviousFaq(e.currentTarget)
   let target = e.currentTarget.parentElement.nextElementSibling;
   const expanded = e.currentTarget.getAttribute('aria-expanded') === 'true' || false
   e.currentTarget.setAttribute('aria-expanded', !expanded)
   target.setAttribute('aria-hidden', expanded)
 }
-function closePreviousFaq() {
+function closePreviousFaq(currAccordion) {
   accordionHeaders.forEach((accordionHeader) => {
+    if(accordionHeader !== currAccordion)
     accordionHeader.setAttribute('aria-expanded',false)
     accordionHeader.parentElement.nextElementSibling.setAttribute('aria-hidden',true)
   })
